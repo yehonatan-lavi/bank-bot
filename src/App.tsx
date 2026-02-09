ab0c7e00960118d310f228d9595d935bcf81b201
YoniBagi <yehonatan.lavi@biocatch.com>
2026-02-09 23:23:51 +0200

    Added logic to create random csid

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Register } from './components/Register';
 import { Transfer } from './components/Transfer';
 import { BotControl } from './components/BotControl';
 import './App.css';
+import {useEffect} from "react";
 
 const AppContent = () => {
   const { currentScreen } = useAppContext();
@@ -19,6 +20,21 @@ const AppContent = () => {
 };
 
 function App() {
+  useEffect(() => {
+    const initBioCatch = () => {
+      const sessionId = `session_${Math.random().toString(36).substr(2, 9)}`;
+
+      if (window.cdApi) {
+        window.cdApi.setCustomerSessionId(sessionId);
+        console.log("BioCatch CSID initialized:", sessionId);
+      } else {
+        //setTimeout(initBioCatch, 500);
+          console.log("BioCatch API not available");
+      }
+    };
+
+    initBioCatch();
+  }, []);
   return (
     <AppProvider>
       <AppContent />
